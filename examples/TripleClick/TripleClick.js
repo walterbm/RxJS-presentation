1f9e0878d366826d1e74f25e2e777ea832811549
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-04-19 01:54:18 -0400

    add triple click example

---
+++ examples/TripleClick/TripleClick.js
@@ -0,0 +1,25 @@
+var button = document.querySelector('.button');
+var label = document.querySelector('h4');
+
+var clickStream = Rx.Observable.fromEvent(button, 'click');
+
+var tripleClickStream = clickStream
+  .buffer(function () {
+    return clickStream.debounce(250)
+  })
+  .map(function(arr) {
+    return arr.length
+  })
+  .filter(function (len) {
+    return len === 3
+  });
+
+tripleClickStream.subscribe(function(event) {
+  label.textContent = 'triple click';
+});
+
+tripleClickStream
+  .debounce(1000)
+  .subscribe(function (suggestion) {
+    label.textContent = '-';
+  });
